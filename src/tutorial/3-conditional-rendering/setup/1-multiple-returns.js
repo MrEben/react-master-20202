f471640433ad94685c580f77ba965b99e800431b
Ebenezer <aniagyeiodame224@gmail.com>
2022-09-06 19:58:10 +0000

    added

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,53 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [loading, setloading] = useState(true);
+  const [isError, setisError] = useState(false);
+  const [user, setuser] = useState("default user");
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status >= 200 && response.status <= 299) {
+          response.json();
+        } else {
+          setloading(false);
+          setisError(true);
+          throw new Error(response.statusText);
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setuser(login);
+        setloading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <div>
+          <h2>Loading...</h2>
+        </div>
+      </>
+    );
+  }
+  if (isError) {
+    return (
+      <>
+        <div>
+          <h1>Error...</h1>
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
+        <h1>{user}</h1>
+      </div>
+    </>
+  );
 };
 
 export default MultipleReturns;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, settext] = useState("");
+  const [error, seterror] = useState(false);
+  // const [text, settext] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h2>{firstValue}</h2>
+      <h3>second : {secondValue} </h3> */}
+      {/* {if () {
+        console.log(see);
+      }} */}
+      <h1>{text || "Hello world"}</h1>
+      <button className="btn" onClick={() => seterror(!error)}>
+        Toggle error
+      </button>
+      {error && <h2>Heyya</h2>}
+      {error ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h3>no error</h3>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
